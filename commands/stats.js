efb4832f0fc37cc242488b45227bfca742199d35
Sanath Jathanna <r20154Sanath@dpsrkp.net>
2020-02-26 14:27:25 +0530

    Beautification of embed

--- commands/stats.js
+++ commands/stats.js
@@ -36,7 +36,7 @@ module.exports = {
 			message.reply('that\'s not a valid date! The correct format is `DD-MM-YYYY`');
 		}
 
-		const row = await database.test(format('SELECT * FROM %s WHERE Date = ?', args[0]), dateStr);
+		const row = await database.test(format('SELECT * FROM %s WHERE Date = ?', 'opgg'), dateStr);
 
 		if (!row) {
 			message.reply('no data was found for ' + dateStr + '!');
@@ -44,29 +44,36 @@ module.exports = {
 			return;
 		}
 
-		// sets desc based on site
-		let desc;
+		// sets image and url based on site
+		let image;
+		let url;
 		switch (args[0]) {
 		case 'opgg':
-			desc = 'Op.gg data';
+			image = 'https://cdn.discordapp.com/attachments/482911683568861186/682143294465245194/Kayle_opgg.png';
+			url = 'https://na.op.gg/champion/kayle/statistics/top/trend';
 			break;
 		case 'log':
-			desc = 'League Of Graphs data';
+			image = '';
+			url = 'https://www.leagueofgraphs.com/champions/stats/kayle';
 			break;
 		case 'lol':
-			desc = 'Lolalytics data';
+			image = '';
+			url = 'https://lolalytics.com/lol/kayle/';
 			break;
 		case 'ugg':
-			desc = 'U.gg data';
+			image = '';
+			url = 'https://u.gg/lol/champions/kayle/build';
 			break;
 		}
 
 		const embed = new Discord.RichEmbed()
 			.setColor('#ff0000')
 			.setTitle('Kayle Data')
-			.setDescription(desc)
-			.addField('Date', row.Date)
-			.addField('Patch', row.Patch)
+			.setURL(url)
+			.setImage(image)
+			.addField('Date', row.Date, true)
+			.addField('Patch', row.Patch, true)
+			.addBlankField()
 			.addField('Winrate', row.Winrate + '%', true)
 			.addField('Pickrate', row.Pickrate + '%', true)
 			.addField('Banrate', row.Banrate + '%', true);
